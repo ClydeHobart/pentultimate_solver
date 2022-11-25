6dafb98f81e97729aaca352a3b9ece5120544c6a
Zeke Baker <zeke.baker@outlook.com>
2022-11-25 16:15:39 -0500

    Added doc comments to match/polyhedra/properties.rs

--- src/math/polyhedra/properties.rs
+++ src/math/polyhedra/properties.rs
@@ -1,30 +1,62 @@
 use crate::math::{polyhedra::Polyhedron, *};
 
+/// Given a `data: polyhedra::data::Data` struct, a `FaceSizeData` struct represents the beginning
+/// of a range of faces within `data.faces` that have the same number of vertices.
 pub struct FaceSizeData {
+    /// The index within the `faces` field of a `Data` instance that `self` refers to
+    ///
+    /// Given two consecutive elements of a `[Self]`, the `initial_face_index` of the latter element
+    /// defines when the range of the former element ends.
     pub initial_face_index: usize,
+
+    /// The number of vertices on any face within the range specified by `self`
+    ///
+    /// The last element in a `[Self]` should have a `face_size` of 0, since it just indicates when
+    /// the range of the previous element ends.
     pub face_size: usize,
 }
 
 impl FaceSizeData {
-    pub const fn new(initial_vert_index: usize, face_size: usize) -> Self {
+    /// A `const` constructor function for a `Self` that doesn't require typing the field names
+    pub const fn new(initial_face_index: usize, face_size: usize) -> Self {
         Self {
-            initial_face_index: initial_vert_index,
+            initial_face_index,
             face_size,
         }
     }
 }
 
+/// A `Properties` struct specifies key defining information for a `Polyhedron`.
 pub struct Properties {
+    /// The `Polyhedron` that `self` describes
     pub polyhedron: Polyhedron,
+
+    /// The number of vertices that the polyhedron has
     pub vert_count: usize,
+
+    /// The number of edges that the polyhedron has
     pub edge_count: usize,
+
+    /// The number of faces that the polyhedron has
     pub face_count: usize,
+
+    /// The `Vec3` vectors needed to construct all the vertices of the polyhedron
     pub base_vectors: &'static [Vec3],
+
+    /// The resultant edge length of the polyhedron using `self.base_vectors`
     pub edge_length: f32,
+
+    /// `FaceSizeData` elements describing how many vertices are on the different faces of the
+    /// polyhedron
+    ///
+    /// Face-transitive polyhedra will have two elements, while non-face-transitive polyhedra will
+    /// have more than two elements (see comment on `FaceSizeData::initial_face_index` for more
+    /// info).
     pub face_sizes: &'static [FaceSizeData],
 }
 
 impl Properties {
+    /// A `const` getter for the corresponding `&'static Self` of a given `Polyhedron` enum.
     pub const fn get(polyhedron: Polyhedron) -> &'static Self {
         match polyhedron {
             Polyhedron::Icosahedron => &ICOSAHEDRON,
@@ -35,6 +67,7 @@ impl Properties {
     }
 }
 
+/// The `Properties` of a regular icosahedron (20 regular triangles)
 pub const ICOSAHEDRON: Properties = Properties {
     polyhedron: Polyhedron::Icosahedron,
     vert_count: 12,
@@ -45,6 +78,7 @@ pub const ICOSAHEDRON: Properties = Properties {
     face_sizes: &[FaceSizeData::new(0, 3), FaceSizeData::new(20, 0)],
 };
 
+/// The `Properties` of a regular dodecahedron (12 regular pentagons)
 pub const DODECAHEDRON: Properties = Properties {
     polyhedron: Polyhedron::Dodecahedron,
     vert_count: ICOSAHEDRON.face_count,
@@ -61,6 +95,7 @@ pub const DODECAHEDRON: Properties = Properties {
     ],
 };
 
+/// The `Properties` of an icosidodecahedron (12 regular pentagons and 20 regular triangles)
 pub const ICOSIDODECAHEDRON: Properties = Properties {
     polyhedron: Polyhedron::Icosidodecahedron,
     vert_count: ICOSAHEDRON.edge_count,
@@ -78,6 +113,7 @@ pub const ICOSIDODECAHEDRON: Properties = Properties {
     ],
 };
 
+/// The `Properties` of a rhombic triacontahedron (30 rhombi)
 pub const RHOMBIC_TRIACONTAHEDRON: Properties = Properties {
     polyhedron: Polyhedron::RhombicTriacontahedron,
     vert_count: ICOSIDODECAHEDRON.face_count,
@@ -95,6 +131,7 @@ pub const RHOMBIC_TRIACONTAHEDRON: Properties = Properties {
     ],
 };
 
+/// The maximum number of vertices of the polyhedra enumerated by `Polyhedron` (32)
 pub const MAX_VERT_COUNT: usize = crate::max!(
     ICOSAHEDRON.vert_count,
     DODECAHEDRON.vert_count,
@@ -102,6 +139,7 @@ pub const MAX_VERT_COUNT: usize = crate::max!(
     RHOMBIC_TRIACONTAHEDRON.vert_count
 );
 
+/// The maximum number of edges of the polyhedra enumerated by `Polyhedron` (60)
 pub const MAX_EDGE_COUNT: usize = crate::max!(
     ICOSAHEDRON.edge_count,
     DODECAHEDRON.edge_count,
